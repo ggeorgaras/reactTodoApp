856c6eba669b3c6070d57e1d436a7121cb2e6888
jinalshah999 <jinalshah999@gmail.com>
2020-03-27 00:23:40 +0530

    done

---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Demo extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
---
+++ src/components/Demo1.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// function Demo1() {
+
+//     return (
+
+//         <div>
+//             <h1>Hello From function Component</h1>
+//         </div>
+//     );
+// }
+
+const Demo1 = (props) => {
+    return (
+        <div>
+            <h1>Hello From function Component</h1>
+            <h2>{props.Title}</h2>
+        </div>
+    );
+
+}
+
+// const Demo1 = () => (
+//     <div>
+//         <h1>Hello From function Component</h1>
+
+//     </div>
+// );
+export default Demo1;
